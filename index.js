cbd4d14372d1b6402447ee3c53b606f6313360d4
Vinicius Gregorio <vincamgreg@hotmail.com>
2023-08-17 10:22:42 -0300

    feat: add mysql

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const mysql = require("mysql");
 
 const app = express();
 const port = 3000;
 
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+
+const conn = mysql.createConnection(config);
+
+const sqlInsert = `INSERT INTO people(name) values('PIPO')`;
+
+conn.query(sqlInsert);
+conn.end();
 app.get("/", (req, res) => {
   res.send("<h1>Docker Container</h1>");
 });
